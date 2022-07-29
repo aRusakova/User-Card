10962a208034ce15f68788658ec5320a7acde65c
Anna Rusakova <vdovina2196@mail.ru>
2022-07-29 21:31:14 +0300

    create project

---
+++ js/script.js
@@ -0,0 +1,15 @@
+const menu = document.querySelector('.menu');
+const menuItem = document.querySelectorAll('.menu__item');
+const hamburger = document.querySelector('.hamburger');
+
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('hamburger_active');
+    menu.classList.toggle('menu_active');
+})
+
+menuItem.forEach(item =>
+    item.addEventListener('click', () => {
+        hamburger.classList.toggle('hamburger_active');
+        menu.classList.toggle('menu_active');
+    })
+);
